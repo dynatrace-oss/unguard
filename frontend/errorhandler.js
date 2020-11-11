78576c83c2b7dbb70e4d473c87cf5d42642ea405
Christoph Wedenig <christoph.wedenig@dynatrace.com>
2020-11-11 17:05:20 +0100

    NOISSUE Cleaned up frontend error handling

--- frontend/errorhandler.js
+++ frontend/errorhandler.js
@@ -1,10 +1,30 @@
 exports.handleError = function (err) {
-    console.error(err)
     let message = "No detailed message available."
-    if (err.response.data && err.response.data.message) {
-        message = err.response.data.message
-        console.error(message)
+    if (!err.response) {
+        console.error(err)
+        return {error: err.code, message: err};
+    }
+    if (err.response.data) {
+        if (err.response.data.message && err.response.data.path) {
+            message = err.response.data.message
+            console.error(`${err.response.data.path} failed (Status: ${err.response.data.status}) with message: ${message}`)
+        } else if (err.response.data.path) {
+            console.error(`${err.response.data.path} failed with status: ${err.response.data.status}`)
+        }
+    } else {
+        console.error(err)
     }
 
     return {error: message, message: err.response.data};
+}
+
+exports.statusCodeForError = function (err) {
+    if (err.response) {
+        if (err.response.status) {
+            return err.response.status
+        }
+        return 500;
+    } else {
+        return 500;
+    }
 }
