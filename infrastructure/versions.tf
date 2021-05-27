e990cabf0119805e0c782b25ec3f196e3b8bc535
Simon Ammer <simon.ammer@dynatrace.com>
2021-05-27 12:25:39 +0200

    CASP-8503 Add a terraform backend

--- infrastructure/versions.tf
+++ infrastructure/versions.tf
@@ -1,6 +1,12 @@
 terraform {
   required_version = ">= 0.15.1"
 
+  backend "s3" {
+    bucket = "ground-zero-infrastructure"
+    key    = "terraform-ecr.tfstate"
+    region = "us-east-1"
+  }
+
   required_providers {
     aws = ">= 3.38.0"
   }
