5ac4e40e676aeecac2cc904e8e07b8eaf65e9b34
Patrick Kern <patrick.kern@dynatrace.com>
2021-07-07 15:16:56 +0200

    CASP-10096 add simple .net5 WebApp with Razor Pages and Docker

---
+++ dotNet-ad-service/dotNet-ad-service/Pages/Ad.cshtml.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace dotNet_ad_service.Pages
+{
+    public class AdModel : PageModel
+    {
+        public string RecourceFolder { get; set; }
+        
+        public void OnGet()
+        {
+            RecourceFolder = "/testFolder";
+            ViewData["resourcePath"] = RecourceFolder;
+        }
+    }
+}
