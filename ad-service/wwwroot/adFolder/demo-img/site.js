14fdb86be2e75a9f6afa117b35ce9daa65c11eba
Patrick Kern <patrick.kern@dynatrace.com>
2021-08-12 08:10:51 +0200

    CASP-10410 add site.js comment to exploit and ad path traversal to original-state.zip

--- ad-service/wwwroot/adFolder/demo-img/site.js
+++ ad-service/wwwroot/adFolder/demo-img/site.js
@@ -1,4 +1,6 @@
-﻿
+﻿/* vulnerable since this file (like every other /wwwroot file) can be overwritten
+ * with the sharpcompress vulnerability */
+
 window.onload = function() {
   // change the ads periodical reload
   setTimeout(() => {
