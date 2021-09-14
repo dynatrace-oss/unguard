258d0ec6cb1a5ff019a54a8a70c78530972db026
Patrick Kern <patrick.kern@dynatrace.com>
2021-09-14 17:20:59 +0200

    CASP-10918: ad-service: fully implement tracing, fix jwt padding bug, add tracing env variable

--- ad-service/Startup.cs
+++ ad-service/Startup.cs
@@ -1,15 +1,18 @@
 using System;
 using System.IO;
+using System.Net.Http;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
+using Flurl;
 using OpenTelemetry.Resources;
 using OpenTelemetry.Trace;
-using OpenTelemetry.Exporter;
-using Flurl;
+
+using AdService.Factory;
 
 namespace AdService
 {
@@ -48,19 +51,43 @@ namespace AdService
                     );
             }
 
+            services.AddSingleton<IHttpContextFactory>(serviceProvider => new W3CHeaderHttpContextFactory(serviceProvider));
+
             services.AddOpenTelemetryTracing(
                 (builder) => builder
                     .SetResourceBuilder(ResourceBuilder
                         .CreateDefault()
                         .AddService(Environment.GetEnvironmentVariable("JAEGER_SERVICE_NAME"))
+                        .AddEnvironmentVariableDetector()
                     )
-                    .AddAspNetCoreInstrumentation()
-                    .AddJaegerExporter()
+                    .AddAspNetCoreInstrumentation()  // Jaeger Trace-Context header compatible through W3CHeaderHttpContextFactory
+                    .AddHttpClientInstrumentation(options => options.Enrich = ((activity, eventName, rawObject) =>
+                        {
+                            if (eventName.Equals("OnStartActivity"))
+                            {
+                                if (rawObject is HttpRequestMessage request)
+                                {
+                                    // Add JAEGER propagation header for other services.
+                                    // uber-trace-id header: $`{ trace-id }:{ span-id }:{ parent-span-id }:{ TraceFlags }`
+                                    // traceparent header: $`{ version-format }-{ trace-id }-{ parent-id (i.e. span-id) }-{ trace-flags }`
+                                    // https://www.jaegertracing.io/docs/1.26/client-libraries/#propagation-format
+
+                                    var jaegerHeader = activity.Context.TraceId
+                                                       + ":" + activity.Context.SpanId
+                                                       + ":" + activity.ParentId
+                                                       + ":" + "0" + (int) activity.ActivityTraceFlags;
+                                    request.Headers.Remove("uber-trace-id");
+                                    request.Headers.Add("uber-trace-id", jaegerHeader);
+                                }
+                            }
+                        })
+
+                    )
+                    .AddJaegerExporter( options =>
+                        options.AgentHost = Environment.GetEnvironmentVariable("JAEGER_AGENT_HOST")
+                    )
+
             );
-            services.Configure<JaegerExporterOptions>(options =>
-            {
-                options.AgentHost = Environment.GetEnvironmentVariable("JAEGER_AGENT_HOST");
-            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
