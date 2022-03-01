348f51bc624a4d2779bdf4044206640c35ea95da
Christoph Wedenig <christoph.wedenig@dynatrace.com>
2022-03-01 12:16:21 +0100

    CASP-13627 Fix log4j not being exploitable

--- src/proxy-service/src/main/java/org/dynatrace/ssrfservice/Application.java
+++ src/proxy-service/src/main/java/org/dynatrace/ssrfservice/Application.java
@@ -8,7 +8,7 @@ import org.springframework.context.ApplicationContext;
 public class Application {
 
     public static void main(String[] args) {
-        ApplicationContext ctx = SpringApplication.run(Application.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
