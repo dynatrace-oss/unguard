1dec58db4e58ee475a55761f748d2f48ab8623f3
Simon Ammer <simon.ammer@dynatrace.com>
2023-03-29 14:52:03 +0200

    👷 ci: Update gitmoji subject-rule to disable lowercase

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,6 @@
-module.exports = {extends: ['gitmoji']};
+module.exports = {
+    extends: ['gitmoji'],
+    rules: {
+        "subject-case": [2, "always", ["sentence-case", "start-case", "pascal-case"]]
+    }
+};
