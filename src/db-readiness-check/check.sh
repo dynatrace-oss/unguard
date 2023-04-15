a6570fea4595e57ffab7b695f2ce56f2e488f4a8
Manuel Fragner <manuel.fragner@dynatrace.com>
2023-04-15 15:06:56 +0200

    Remove db-readiness check

--- src/db-readiness-check/check.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh -e
-echo "Trying to connect to mysql:";
-
-until mysql --host="$MYSQL_HOST" --user="$MYSQL_USER" --password="$MYSQL_PASSWORD" --execute="SELECT 1";
-  do
-      echo "Waiting for mysql...";
-      sleep 2;
-  done;
