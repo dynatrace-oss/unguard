29c9b341d865997cea4c8731c6d58b508b38e78a
eliasgierlinger <gierlinger.elias@gmail.com>
2023-10-09 16:43:14 +0200

    📄 refactor(like-service): Add license to all PHP files

--- src/like-service/app/Http/Kernel.php
+++ src/like-service/app/Http/Kernel.php
@@ -1,4 +1,19 @@
 <?php
+#
+# Copyright 2023 Dynatrace LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
 namespace App\Http;
 
@@ -14,7 +29,6 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $middleware = [
-        // \App\Http\Middleware\TrustHosts::class,
         \App\Http\Middleware\TrustProxies::class,
         \Fruitcake\Cors\HandleCors::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
@@ -34,12 +48,7 @@ class Kernel extends HttpKernel
             \Illuminate\Session\Middleware\StartSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-        ],
-
-        //'api' => [
-        //    'throttle:api',
-        //    \Illuminate\Routing\Middleware\SubstituteBindings::class,
-        //],
+        ]
     ];
 
     /**
