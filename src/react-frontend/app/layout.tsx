d60d5ecb8846299773d890853df2b8e7cd72c7e2
Laura Randl <laura.randl@dynatrace.com>
2025-03-18 17:37:07 +0100

    :tada: feat(frontend): Set up new Next.js app

---
+++ src/react-frontend/app/layout.tsx
@@ -0,0 +1,47 @@
+import "@/styles/globals.css";
+import { Metadata, Viewport } from "next";
+
+import { Providers } from "./providers";
+
+import { siteConfig } from "@/config/site";
+
+export const metadata: Metadata = {
+  title: {
+    default: siteConfig.name,
+    template: `%s - ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
+
+export const viewport: Viewport = {
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "white" },
+    { media: "(prefers-color-scheme: dark)", color: "black" },
+  ],
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html suppressHydrationWarning lang="en">
+      <head><title></title></head>
+      <body>
+        <Providers themeProps={{ attribute: "class", defaultTheme: "light" }}>
+          <div className="relative flex flex-col h-screen">
+            <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
+              {children}
+            </main>
+            <footer className="w-full flex items-center justify-center py-3">
+            </footer>
+          </div>
+        </Providers>
+      </body>
+    </html>
+  );
+}
