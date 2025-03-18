5c0ae0defcdc2f8ed33d7fe0145898358ec9e1cc
Laura Randl <laura.randl@dynatrace.com>
2025-03-18 17:37:07 +0100

    :tada: feat(frontend): Set up new Next.js app

---
+++ src/react-frontend/config/site.ts
@@ -0,0 +1,12 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "Unguard",
+  description: "An insecure cloud-native microservices demo application. It consists of eight app services, a load generator, and two databases.",
+  navItems: [
+    {
+      label: "Home",
+      href: "/",
+    },
+  ]
+};
