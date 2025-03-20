c8d1c6a5a103885509892a04d31ddc6aa819325c
Laura Randl <laura.randl@dynatrace.com>
2025-03-20 16:49:04 +0100

    :sparkles: feat(frontend): Containerized Next.js app & basic layout

---
+++ src/frontend-nextjs/components/Footer.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { Link } from "@heroui/react";
+
+export default function Footer() {
+    return (
+        <div>
+            <p>Powered by Unguard.</p>
+            <p>
+                Robots lovingly delivered by{" "}
+                <Link href={"https://robohash.org/"}>Robohash.org</Link>
+            </p>
+        </div>
+    );
+}
--- src/react-frontend/next.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    basePath: '/ui'
-};
-
-module.exports = nextConfig;
