98e61af041020a56aaa94764a1a90d94d55141ee
Laura Randl <laura.randl@dynatrace.com>
2025-03-27 10:00:32 +0100

    :art: style(frontend): Reformatted files with prettier

--- src/frontend-nextjs/app/api/test/route.ts
+++ src/frontend-nextjs/app/api/test/route.ts
@@ -1,8 +1,8 @@
 export async function GET(request: Request) {
-    const testObject = [{ id: 1, name: "Test!" }];
+    const testObject = [{ id: 1, name: 'Test!' }];
 
     return new Response(JSON.stringify(testObject), {
         status: 200,
-        headers: { "Content-Type": "application/json" },
+        headers: { 'Content-Type': 'application/json' },
     });
 }
--- src/frontend-nextjs/tailwind.config.js
+++ src/frontend-nextjs/tailwind.config.js
@@ -1,22 +1,22 @@
-import {heroui} from "@heroui/theme"
+import { heroui } from '@heroui/theme';
 
 /** @type {import('tailwindcss').Config} */
 const config = {
-  content: [
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}"
-  ],
-  theme: {
-    extend: {
-        colors: {
-            primary: "#4ab973",
-            secondary: "#f3f3f3",
-        }
+    content: [
+        './components/**/*.{js,ts,jsx,tsx,mdx}',
+        './app/**/*.{js,ts,jsx,tsx,mdx}',
+        './node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}',
+    ],
+    theme: {
+        extend: {
+            colors: {
+                primary: '#4ab973',
+                secondary: '#f3f3f3',
+            },
+        },
     },
-  },
-  darkMode: "class",
-  plugins: [heroui()],
-}
+    darkMode: 'class',
+    plugins: [heroui()],
+};
 
 module.exports = config;
