64ee1a494866958dca2d7a12efcbeba0e96845a9
Laura Randl <laura.randl@dynatrace.com>
2025-03-27 10:00:32 +0100

    :art: style(frontend): Reformatted files with prettier

--- src/frontend-nextjs/app/providers.tsx
+++ src/frontend-nextjs/app/providers.tsx
@@ -1,22 +1,20 @@
-"use client";
+'use client';
 
-import type { ThemeProviderProps } from "next-themes";
+import type { ThemeProviderProps } from 'next-themes';
 
-import * as React from "react";
-import { HeroUIProvider } from "@heroui/system";
-import { useRouter } from "next/navigation";
-import { ThemeProvider as NextThemesProvider } from "next-themes";
+import * as React from 'react';
+import { HeroUIProvider } from '@heroui/system';
+import { useRouter } from 'next/navigation';
+import { ThemeProvider as NextThemesProvider } from 'next-themes';
 
 export interface ProvidersProps {
     children: React.ReactNode;
     themeProps?: ThemeProviderProps;
 }
 
-declare module "@react-types/shared" {
+declare module '@react-types/shared' {
     interface RouterConfig {
-        routerOptions: NonNullable<
-            Parameters<ReturnType<typeof useRouter>["push"]>[1]
-        >;
+        routerOptions: NonNullable<Parameters<ReturnType<typeof useRouter>['push']>[1]>;
     }
 }
 
--- src/frontend-nextjs/components/NavigationBar.tsx
+++ src/frontend-nextjs/components/NavigationBar.tsx
@@ -1,25 +1,10 @@
-"use client";
+'use client';
 
-import {
-    Navbar,
-    NavbarBrand,
-    NavbarContent,
-    NavbarItem,
-    Link,
-    Button,
-    Image,
-} from "@heroui/react";
-import { usePathname } from "next/navigation";
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button, Image } from '@heroui/react';
+import { usePathname } from 'next/navigation';
 
 export function UnguardLogo() {
-    return (
-        <Image
-            alt="Unguard Logo"
-            height="32"
-            src={`/ui/unguard_logo.svg`}
-            width="32"
-        />
-    );
+    return <Image alt='Unguard Logo' height='32' src='/ui/unguard_logo.svg' width='32' />;
 }
 
 export default function NavigationBar() {
@@ -27,56 +12,41 @@ export default function NavigationBar() {
 
     return (
         <Navbar
-            maxWidth={"full"}
+            maxWidth='full'
             style={{
-                backgroundColor: "#4ab973",
-                color: "#f1f1f1",
-                borderRadius: "15px",
+                backgroundColor: '#4ab973',
+                color: '#f1f1f1',
+                borderRadius: '15px',
             }}
         >
-            <NavbarBrand className="max-w-36">
+            <NavbarBrand className='max-w-36'>
                 <UnguardLogo />
-                <p className="font-bold text-inherit px-2 text-large">
-                    Unguard
-                </p>
+                <p className='font-bold text-inherit px-2 text-large'>Unguard</p>
             </NavbarBrand>
-            <NavbarContent className="hidden sm:flex gap-4" justify="center">
+            <NavbarContent className='hidden sm:flex gap-4' justify='center'>
                 <NavbarItem>
-                    <Link
-                        className={`${pathname === "/" && "font-extrabold"}`}
-                        color="secondary"
-                        href="/"
-                    >
+                    <Link className={`${pathname === '/' && 'font-extrabold'}`} color='secondary' href='/'>
                         Home
                     </Link>
                 </NavbarItem>
                 <NavbarItem>
-                    <Link
-                        className={`${pathname === "/users" && "font-extrabold"}`}
-                        color="secondary"
-                        href="/users"
-                    >
+                    <Link className={`${pathname === '/users' && 'font-extrabold'}`} color='secondary' href='/users'>
                         Users
                     </Link>
                 </NavbarItem>
                 <NavbarItem>
                     <Link
-                        className={`${pathname === "/mytimeline" && "font-extrabold"}`}
-                        color="secondary"
-                        href="/mytimeline"
+                        className={`${pathname === '/mytimeline' && 'font-extrabold'}`}
+                        color='secondary'
+                        href='/mytimeline'
                     >
                         My Timeline
                     </Link>
                 </NavbarItem>
             </NavbarContent>
-            <NavbarContent justify="end">
-                <NavbarItem className="justify-items-end">
-                    <Button
-                        as={Link}
-                        color="default"
-                        href="/login"
-                        variant="solid"
-                    >
+            <NavbarContent justify='end'>
+                <NavbarItem className='justify-items-end'>
+                    <Button as={Link} color='default' href='/login' variant='solid'>
                         Login/Register
                     </Button>
                 </NavbarItem>
