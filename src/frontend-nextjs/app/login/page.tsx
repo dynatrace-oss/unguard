5eb03ddc174671269697aa45ad64c39ddf71f232
Laura Randl <laura.randl@dynatrace.com>
2025-04-03 17:15:15 +0200

    :sparkles: feat(frontend): API routes for login & register

--- src/frontend-nextjs/app/login/page.tsx
+++ src/frontend-nextjs/app/login/page.tsx
@@ -1,35 +1,65 @@
 'use client';
 
-import { Form, Image, Input, Button, Spacer } from '@heroui/react';
 import React, { useState } from 'react';
+import { Image, Button, Form, Input, Spacer } from '@heroui/react';
+import { useRouter } from 'next/navigation';
 
-export function UnguardLogo() {
-    return (
-        <Image
-            alt='Unguard Logo'
-            className='justify-content-center'
-            height='55'
-            src='/ui/unguard_logo_black.svg'
-            width='55'
-        />
-    );
+import { ROUTES } from '@/app/enums/routes';
+
+async function register(data: {}) {
+    const res = await fetch('api/auth/register', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data),
+    });
+
+    return res;
 }
 
-async function register(data: any) {
-    //TODO
+async function login(data: {}) {
+    const res = await fetch('api/auth/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data),
+    });
+
+    //TODO: store JWT
+
+    return res;
 }
 
-function login(data: {}) {
-    //TODO
+function getErrorMsg(code: number): string {
+    switch (code) {
+        case 400:
+            return 'Only numbers, underscores, upper- and lowercase letters are allowed in the username.';
+        case 401:
+            return 'Incorrect username or password';
+        case 404:
+            return 'Incorrect username or password.';
+        case 409:
+            return 'User with this username already exists';
+        case 500:
+            return 'Error registering or logging in.';
+        default:
+            return 'An unexpected error occurred. Please try again.';
+    }
 }
 
 export default function LoginRegister() {
     const [isLogin, setIsLogin] = useState(true); //if false, is register
+    const [errorMsg, setErrorMsg] = useState('');
+    const router = useRouter();
 
     return (
         <div className='flex flex-col items-center align-items-center justify-center'>
             <Spacer y={6} />
-            <UnguardLogo />
+            <Image
+                alt='Unguard Logo'
+                className='justify-content-center'
+                height='55'
+                src='/ui/unguard_logo_black.svg'
+                width='55'
+            />
             <Spacer y={6} />
             <Form
                 className=' w-full max-w-xs flex flex-col gap-4 items-center'
@@ -38,9 +68,31 @@ export default function LoginRegister() {
                     let data = Object.fromEntries(new FormData(e.currentTarget));
 
                     if (isLogin) {
-                        login(data);
+                        login(data).then((res) => {
+                            if (!res.ok) {
+                                setErrorMsg(getErrorMsg(res.status));
+                            } else {
+                                setErrorMsg('');
+                                router.push(ROUTES.home);
+                            }
+                        });
                     } else {
-                        register({ data: data });
+                        register(data).then((res) => {
+                            if (!res.ok) {
+                                setErrorMsg(getErrorMsg(res.status));
+                                throw new Error('Error creating new user');
+                            } else {
+                                setErrorMsg('');
+                                login(data).then((res) => {
+                                    if (!res.ok) {
+                                        setErrorMsg(getErrorMsg(res.status));
+                                        throw new Error('Error logging in new user');
+                                    } else {
+                                        router.push(ROUTES.home);
+                                    }
+                                });
+                            }
+                        });
                     }
                 }}
             >
@@ -61,6 +113,7 @@ export default function LoginRegister() {
                     type='password'
                     variant='flat'
                 />
+                {errorMsg.length > 0 && <p className='text-red-700 font-bold'>{errorMsg}</p>}
                 <div className='flex gap-2'>
                     <Button color='primary' type='submit' onPress={() => setIsLogin(true)}>
                         Login
