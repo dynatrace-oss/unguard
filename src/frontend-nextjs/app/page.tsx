bbfb3a2c808d4aaee40971554046899c3ef9485e
Laura Randl <laura.randl@dynatrace.com>
2025-04-08 12:45:08 +0200

    :sparkles: feat(frontend): Incorporated review suggestions

--- src/frontend-nextjs/app/page.tsx
+++ src/frontend-nextjs/app/page.tsx
@@ -1,5 +1,5 @@
-import Timeline from '@/components/Timeline';
-import AdComponent from '@/components/AdComponent';
+import { Timeline } from '@/components/Timeline';
+import { Ad } from '@/components/Ad';
 
 export default function Home() {
     return (
@@ -7,7 +7,7 @@ export default function Home() {
             <h1 className='mb-6 text-4xl font-extrabold leading-none tracking-tight text-gray-800'>Timeline</h1>
             <div className='grid gap-16 grid-cols-[70%_30%] min-h-screen'>
                 <Timeline />
-                <AdComponent />
+                <Ad />
             </div>
         </div>
     );
