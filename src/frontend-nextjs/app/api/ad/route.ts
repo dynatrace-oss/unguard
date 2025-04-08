a1fb362e401b286e44a86fb4b04bc6986d95164d
Laura Randl <laura.randl@dynatrace.com>
2025-04-08 12:45:08 +0200

    :sparkles: feat(frontend): Incorporated review suggestions

--- src/frontend-nextjs/app/api/ad/route.ts
+++ src/frontend-nextjs/app/api/ad/route.ts
@@ -1,13 +1,5 @@
 import { NextResponse } from 'next/server';
 
-export async function GET(request: Request) {
-    return new NextResponse(
-        JSON.stringify({
-            src: '/ad-service',
-        }),
-        {
-            status: 200,
-            headers: { 'Content-Type': 'application/json' },
-        },
-    );
+export async function GET(): Promise<NextResponse> {
+    return NextResponse.json({ src: '/ad-service' }, { status: 200 });
 }
