f53b1d84d479c5ffbdb513e8bde5e48891d65cc2
Laura Randl <laura.randl@dynatrace.com>
2025-04-10 13:49:49 +0200

    :sparkles: feat(frontend): Toasts & some improvements

--- src/frontend-nextjs/app/providers.tsx
+++ src/frontend-nextjs/app/providers.tsx
@@ -7,16 +7,12 @@ import * as React from 'react';
 import { HeroUIProvider } from '@heroui/system';
 import { useRouter } from 'next/navigation';
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
+import { useState } from 'react';
+import { ToastProvider } from '@heroui/react';
 
 export interface ProvidersProps {
     children: React.ReactNode;
     themeProps?: ThemeProviderProps;
-}
-
-declare module '@react-types/shared' {
-    interface RouterConfig {
-        routerOptions: NonNullable<Parameters<ReturnType<typeof useRouter>['push']>[1]>;
-    }
 }
 
 export function Providers({ children, themeProps }: ProvidersProps) {
@@ -26,6 +22,7 @@ export function Providers({ children, themeProps }: ProvidersProps) {
     return (
         <QueryClientProvider client={queryClient}>
             <HeroUIProvider navigate={router.push}>
+                <ToastProvider toastProps={{ color: 'primary' }} />
                 <NextThemesProvider {...themeProps}>{children}</NextThemesProvider>
             </HeroUIProvider>
         </QueryClientProvider>
--- src/frontend-nextjs/hooks/useCheckLogin.ts
+++ src/frontend-nextjs/hooks/useCheckLogin.ts
@@ -1,7 +1,7 @@
 import { useQuery } from '@tanstack/react-query';
 
 async function fetchIsLoggedIn() {
-    const res = await fetch('ui/api/auth/login', { method: 'GET' });
+    const res = await fetch('/ui/api/auth/login', { method: 'GET' });
 
     if (!res.ok) {
         throw new Error('Failed to fetch login status');
