63612f676139e19addf4ded34810fbb9024884f1
Laura Randl <laura.randl@dynatrace.com>
2025-04-22 17:08:29 +0200

    :sparkles: feat(frontend): Suggestions from review & fixes after rebase

--- src/frontend-nextjs/app/api/auth/register/route.ts
+++ src/frontend-nextjs/app/api/auth/register/route.ts
@@ -3,7 +3,11 @@ import { NextResponse } from 'next/server';
 import { USER_AUTH_API } from '@/axios';
 
 async function registerUser(user: { username: string; password: string }) {
-    const res_user = await USER_AUTH_API.post('/user/register', user).catch();
+    const res_user = await USER_AUTH_API.get('/user/register', {
+        params: {
+            username: user.username,
+            password: user.password}
+    }).catch();
 
     if (res_user.status !== 200) {
         throw new Error('Failed to register new user');
