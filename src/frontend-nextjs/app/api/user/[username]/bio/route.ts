2460ea07a9258d35f6d1c161091afb61f88495f2
Laura Randl <laura.randl@dynatrace.com>
2025-04-22 13:54:10 +0200

    :sparkles: feat(frontend): BioEditor for own user profile

--- src/frontend-nextjs/app/api/user/[username]/bio/route.ts
+++ src/frontend-nextjs/app/api/user/[username]/bio/route.ts
@@ -17,12 +17,42 @@ export async function GET(req: Request, { params }: { params: Promise<{ username
     try {
         const res_bio = await fetchBio(res_userid.userId);
 
-        return NextResponse.json(res_bio, { status: 200 });
+        return NextResponse.json(res_bio.bioText, { status: 200 });
     } catch (error: any) {
         if (error.response && error.response.status == 404) {
+            //bio can be empty
             return NextResponse.json('', { status: 200 });
         } else {
             return NextResponse.json('Error retrieving bio', { status: 500 });
         }
     }
+}
+
+async function editBio(userid: string, body: { bioText: string; enableMarkdown: boolean }) {
+    const res_user = await PROFILE_SERVICE.post(
+        `/user/${userid}/bio`,
+        {},
+        {
+            params: {
+                bioText: body.bioText,
+                enableMarkdown: Boolean(body.enableMarkdown),
+            },
+        },
+    );
+
+    if (res_user.status !== 200) {
+        throw new Error('Failed to edit bio of user ');
+    }
+
+    return res_user.data;
+}
+
+export async function POST(req: Request, { params }: { params: Promise<{ username: string }> }): Promise<NextResponse> {
+    const { username } = await params;
+    const res_userid = await fetchUserId(username);
+
+    const body = await req.json();
+    const res_bio = await editBio(res_userid.userId, body);
+
+    return NextResponse.json(res_bio, { status: res_bio.status });
 }
