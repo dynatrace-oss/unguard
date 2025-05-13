4e795efce3a9492e3cd5b424398842264f1cd935
Laura Randl <laura.randl@dynatrace.com>
2025-05-13 16:42:53 +0200

    :sparkles: feat(frontend): Some fixes after rebasing

--- src/frontend-nextjs/hooks/usePosts.ts
+++ src/frontend-nextjs/hooks/usePosts.ts
@@ -2,16 +2,6 @@ import { useQuery } from '@tanstack/react-query';
 
 import { QUERY_KEYS } from '@/enums/queryKeys';
 
-async function fetchPostsOfUser(username: string) {
-    const res = await fetch(`/ui/api/posts/${username}`);
-
-    if (!res.ok) {
-        throw new Error('Failed to fetch posts for user ' + username);
-    }
-
-    return res.json();
-}
-
 async function fetchAllPosts() {
     const res = await fetch('/ui/api/posts');
 
