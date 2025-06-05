43b98e8458e539992eaf6eecc97a28b17e23cc9e
Laura Randl <laura.randl@dynatrace.com>
2025-06-05 13:37:51 +0200

    :sparkles: feat(frontend): Membership plans page design

---
+++ src/frontend-nextjs/app/membership-plans/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import { MembershipSelector } from '@/components/MembershipSelector';
+
+export default async function MembershipPlans() {
+    return <MembershipSelector />;
+}
