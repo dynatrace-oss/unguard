84866299672e16aee62f2dd76c6f700c3d854200
Laura Randl <laura.randl@dynatrace.com>
2025-06-17 16:52:11 +0200

    :sparkles: feat(frontend): Further improvements from review

--- src/frontend-nextjs/hooks/useUpdatePaymentInfo.ts
+++ src/frontend-nextjs/hooks/useUpdatePaymentInfo.ts
@@ -1,32 +1,19 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
-import { addToast } from '@heroui/react';
 
 import { PaymentData, updatePaymentData } from '@/services/PaymentService';
 import { QUERY_KEYS } from '@/enums/queryKeys';
 
-export function useUpdatePaymentInfo(username: string, setErrorMsg: (msg: string) => void) {
+export function useUpdatePaymentInfo(username: string, onSuccess: () => void, onError: (error: any) => void) {
     const queryClient = useQueryClient();
 
     return useMutation({
         mutationFn: (paymentData: PaymentData) => updatePaymentData(paymentData, username),
-        onSuccess: async () => {
-            setErrorMsg('');
-            addToast({
-                color: 'success',
-                title: 'Success',
-                description: 'Your Payment Info is now up to date!',
-            });
-            await queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.payment_data, username] });
+        onSuccess: () => {
+            onSuccess();
+            queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.payment_data, username] });
         },
         onError: (error: any) => {
-            const errorMessage = error.message || 'Error updating payment data';
-
-            setErrorMsg(errorMessage);
-            addToast({
-                color: 'danger',
-                title: 'Failed to update payment information',
-                description: errorMessage,
-            });
+            onError(error);
         },
     });
 }
