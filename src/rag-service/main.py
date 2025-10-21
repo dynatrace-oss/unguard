32795479083c92a5faa5d55c749efbd25410be1e
Laura Randl <laura.randl@dynatrace.com>
2025-10-21 09:43:54 +0200

    :sparkles: feat(rag-service): Add logging

--- src/rag-service/main.py
+++ src/rag-service/main.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI
 from config import settings
+from logging_config import get_logger
 
 from routers import healtz, ingestion, classification
 
 app = FastAPI(title=settings.app_name, description=settings.app_description)
+_logger = get_logger(__name__)
+
+@app.on_event("startup")
+async def startup():
+    _logger.info("rag-service starting")
+
+@app.on_event("shutdown")
+async def shutdown():
+    _logger.info("rag-service shutting down")
 
 app.include_router(classification.router)
 app.include_router(ingestion.router)
