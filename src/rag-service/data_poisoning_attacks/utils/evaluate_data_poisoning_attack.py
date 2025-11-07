6ad4c629565f2625a741f9dedcd9ddfbe0ff647b
Laura Randl <laura.randl@dynatrace.com>
2025-11-07 11:00:05 +0100

    :sparkles: feat(rag-service): Add label flipping attack script

---
+++ src/rag-service/data_poisoning_attacks/utils/evaluate_data_poisoning_attack.py
@@ -0,0 +1,9 @@
+
+def evaluate_model_performance():
+    """Evaluate the model performance after a data poisoning attack."""
+    # TODO
+
+def evaluate_attack_success_rate():
+    """Evaluate the success rate of the data poisoning attack."""
+    # TODO
+
