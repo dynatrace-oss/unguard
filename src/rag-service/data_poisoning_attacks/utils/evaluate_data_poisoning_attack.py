efe69443b800b8dcfa7539dc234d0809766aa436
Laura Randl <laura.randl@dynatrace.com>
2025-11-07 13:20:42 +0100

    :sparkles: feat(rag-service): Evaluate model performance after poisoning attack

--- src/rag-service/data_poisoning_attacks/utils/evaluate_data_poisoning_attack.py
+++ src/rag-service/data_poisoning_attacks/utils/evaluate_data_poisoning_attack.py
@@ -1,9 +1,13 @@
+from evaluation.evaluate_model import evaluate_model
+from pathlib import Path
+
+ATTACK_EVALUATION_RESULTS_DIR: Path = Path(__file__).parent / "evaluation_results"
+
 
 def evaluate_model_performance():
     """Evaluate the model performance after a data poisoning attack."""
-    # TODO
+    evaluate_model(ATTACK_EVALUATION_RESULTS_DIR)
 
 def evaluate_attack_success_rate():
     """Evaluate the success rate of the data poisoning attack."""
     # TODO
-
