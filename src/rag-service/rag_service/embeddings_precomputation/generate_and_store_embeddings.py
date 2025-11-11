25fc10e9ee9de4271fb0737d90a202b96522d066
Laura Randl <laura.randl@dynatrace.com>
2025-11-11 16:31:35 +0100

    :sparkles: feat(rag-service): Prepare data for label flipping attack

--- src/rag-service/rag_service/embeddings_precomputation/generate_and_store_embeddings.py
+++ src/rag-service/rag_service/embeddings_precomputation/generate_and_store_embeddings.py
@@ -4,16 +4,16 @@ from llama_index.core import Document
 
 from logger.logging_config import get_logger
 from rag_service.config import get_settings
-from rag_service.data_loader.parquet_data_loader import DataLoader
 from rag_service.embeddings_precomputation.utils.generate_embeddings import create_embedding_model, compute_embeddings_for_batch
 from rag_service.embeddings_precomputation.utils.write_embeddings_to_files import EmbeddingsWriter, compute_entry_id
 
 logger = get_logger("EmbeddingsPreComputer")
 settings = get_settings()
 
-def generate_and_store_embeddings(docs: List[Document], embeddings_store_dir_path: Path, batch_size: int = 200) -> int:
+def generate_and_store_embeddings(docs: List[Document], embeddings_store_dir_path: Path) -> int:
     """Computes the embeddings for the provided docs and stores them in multiple files in the provided directory."""
-    logger.info("Generating embeddings for %d documents (batch_size=%d)...", len(docs), batch_size)
+    batch_size = settings.embeddings_computation_max_batch_size
+    logger.info("Precomputing embeddings for %d documents (batch_size=%d)...", len(docs), batch_size)
 
     embedding_model = create_embedding_model()
     writer = EmbeddingsWriter(embeddings_store_dir_path)
@@ -45,15 +45,3 @@ def generate_and_store_embeddings(docs: List[Document], embeddings_store_dir_pat
     logger.info("Pre-computing complete: %d embeddings written across %d files at directory %s)",
         num_of_embedded_docs, writer.file_index - 1, embeddings_store_dir_path)
     return num_of_embedded_docs
-
-def precompute_base_embeddings() -> int:
-    """Computes and stores embeddings for the initial KB data."""
-    docs = DataLoader().load_initial_kb_data()
-    return generate_and_store_embeddings(
-        docs=docs,
-        embeddings_store_dir_path=settings.base_embeddings_store_path,
-        batch_size=settings.embeddings_computation_max_batch_size,
-    )
-
-if __name__ == "__main__":
-    precompute_base_embeddings()
