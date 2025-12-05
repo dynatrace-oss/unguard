a9408f69f34cbc3ec4841b325a57140442dce492
Laura Randl <laura.randl@dynatrace.com>
2025-12-05 10:03:37 +0100

    :sparkles: feat(rag-service): Add precomputed embeddings for legit data ingestion

--- src/rag-service/data_preprocessing/prepare_datasets.py
+++ src/rag-service/data_preprocessing/prepare_datasets.py
@@ -3,8 +3,6 @@ from llama_index.core import Document
 
 from data_preprocessing.prepare_attack_datasets.prepare_keyword_attack_datasets import prepare_keyword_attack_datasets
 from data_preprocessing.prepare_attack_datasets.prepare_label_flipping_dataset import prepare_label_flipping_dataset
-from data_preprocessing.prepare_attack_datasets.prepare_legit_data_ingestion_dataset import \
-    prepare_legit_data_ingestion_dataset
 from logger.logging_config import get_logger
 from rag_service.config import get_settings
 from rag_service.data_loader.parquet_data_loader import DataLoader
@@ -18,16 +16,21 @@ def prepare_initial_kb_dataset(docs: List[Document]):
     logger.info("Preparing initial KB dataset for %d documents...", len(docs))
     return generate_and_store_embeddings(docs, settings.base_embeddings_store_path)
 
+def prepare_legit_data_ingestion_dataset(docs: List[Document]) -> int:
+    """Prepares the dataset for a legit data ingestion by precomputing embeddings"""
+    logger.info("Preparing legit data ingestion dataset for %d documents...", len(docs))
+    return generate_and_store_embeddings(docs, settings.legit_data_embeddings_store_path)
+
 def prepare_datasets():
     """Prepares the datasets for the initial KB data and attack scripts with precomputed embeddings"""
     docs = DataLoader().load_initial_kb_data()
     docs_initial_kb_data = docs[:len(docs)//2]
-    docs_attack_scripts_data = docs[len(docs)//2:]
+    ingestion_docs = docs[len(docs)//2:]
 
     prepare_initial_kb_dataset(docs_initial_kb_data)
-    prepare_label_flipping_dataset(docs_attack_scripts_data)
-    prepare_keyword_attack_datasets(docs_attack_scripts_data)
-    prepare_legit_data_ingestion_dataset(docs_attack_scripts_data)
+    prepare_label_flipping_dataset(ingestion_docs)
+    prepare_keyword_attack_datasets(ingestion_docs)
+    prepare_legit_data_ingestion_dataset(ingestion_docs)
 
 if __name__ == "__main__":
     prepare_datasets()
