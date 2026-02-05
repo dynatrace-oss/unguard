f24897350a3d7baf275846cd2cfad5e79231f0c2
Laura Randl <laura.randl@dynatrace.com>
2026-02-05 13:31:55 +0100

    :sparkles: feat: Add user ratings for spam predictions

--- src/frontend-nextjs/components/Timeline/PostSpamPrediction.tsx
+++ src/frontend-nextjs/components/Timeline/PostSpamPrediction.tsx
@@ -1,33 +1,37 @@
+'use client';
+
 import React from 'react';
 import {Alert} from "@heroui/react";
 
+import {SpamPredictionUserRating} from "@/components/Timeline/SpamPredictionUserRating";
+import {useCheckLogin} from "@/hooks/queries/useCheckLogin";
+import {ErrorBoundary} from "react-error-boundary";
+import {ErrorCard} from "@/components/ErrorCard";
+
 export interface PostSpamPredictionProps {
     isSpamPredictedLabel?: boolean | null;
+    postId: string;
 }
 
-
-
 export function PostSpamPrediction(props: Readonly<PostSpamPredictionProps>) {
-    console.log("PostSpamPrediction props:", props);
+    const { isLoggedIn } = useCheckLogin();
     if (props.isSpamPredictedLabel == null) return null;
 
+    const color = props.isSpamPredictedLabel ? 'danger' : 'primary';
 
-    if (props.isSpamPredictedLabel) {
-        const color='danger'
-        return (
-            <div key={color} className='w-full flex items-center my-3'>
-                <div>
-                    <Alert color={color} title={'Potential Spam Detected'} />
-                </div>
-            </div>
-        );
-    }
-
-    const color = 'primary'
     return (
         <div key={color} className='w-full flex items-center my-3'>
-            <div>
-                <Alert color={color} title={'No Spam Detected'} />
+            <div className="w-full">
+                <Alert color={color}>
+                    <div className="flex w-full items-center justify-between gap-3">
+                        <div className="font-semibold">{props.isSpamPredictedLabel? "Potential Spam Detected" : "No Spam Detected"}</div>
+                        {isLoggedIn && (
+                            <ErrorBoundary fallbackRender={(props) => <ErrorCard message={props.error.message} />}>
+                                <SpamPredictionUserRating isSpamPredictedLabel={props.isSpamPredictedLabel} postId={props.postId} />
+                            </ErrorBoundary>
+                        )}
+                    </div>
+                </Alert>
             </div>
         </div>
     );
