ef43a92e12ea2552a291b74f9836a796021c2341
Laura Randl <laura.randl@dynatrace.com>
2026-02-10 16:47:43 +0100

    :sparkles: feat(feedback-ingestion-service): Ingest buffered user feedback in regular intervals

--- src/feedback-ingestion-service/src/main/java/org/dynatrace/feedbackingestionservice/FeedbackIngestionController.java
+++ src/feedback-ingestion-service/src/main/java/org/dynatrace/feedbackingestionservice/FeedbackIngestionController.java
@@ -1,49 +1,35 @@
 package org.dynatrace.feedbackingestionservice;
 
 import org.dynatrace.feedbackingestionservice.dto.UserFeedback;
-import org.dynatrace.feedbackingestionservice.exceptions.*;
-import org.dynatrace.feedbackingestionservice.ragservice.RAGServiceClient;
+import org.dynatrace.feedbackingestionservice.buffer.FeedbackBuffer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
+import static org.dynatrace.feedbackingestionservice.utils.UserFeedbackValidator.validateUserFeedback;
 
 @RestController
 public class FeedbackIngestionController {
-    Logger logger = LoggerFactory.getLogger(FeedbackIngestionController.class);
-    private final RAGServiceClient ragServiceClient;
-    private final ExecutorService ragExecutor = Executors.newFixedThreadPool(
-        Math.max(10, Runtime.getRuntime().availableProcessors() / 2)
-    );
 
-    public FeedbackIngestionController() {
-        String ragServiceAddress;
-        String ragServicePort;
+    private final Logger logger = LoggerFactory.getLogger(FeedbackIngestionController.class);
+    private final FeedbackBuffer buffer;
 
-        if (System.getenv("RAG_SERVICE_ADDRESS") != null) {
-            ragServiceAddress = System.getenv("RAG_SERVICE_ADDRESS");
-            logger.info("RAG_SERVICE_ADDRESS set to {}", ragServiceAddress);
-        } else {
-            ragServiceAddress = "localhost";
-            logger.warn("No RAG_SERVICE_ADDRESS environment variable defined, falling back to localhost.");
-        }
-        if (System.getenv("RAG_SERVICE_PORT") != null) {
-            ragServicePort = System.getenv("RAG_SERVICE_PORT");
-            logger.info("RAG_SERVICE_PORT set to {}", ragServicePort);
-        } else {
-            ragServicePort = "8000";
-            logger.warn("No RAG_SERVICE_PORT environment variable defined, falling back to 8000.");
-        }
-
-        this.ragServiceClient = new RAGServiceClient(ragServiceAddress, ragServicePort);
+    public FeedbackIngestionController(FeedbackBuffer buffer) {
+        this.buffer = buffer;
     }
 
-    @RequestMapping("/add")
-    public void add(UserFeedback userFeedback) throws IngestionFailedException {
-        //TODO: implement
+    @PostMapping("/add")
+    public ResponseEntity<Void> add(@RequestBody UserFeedback userFeedback) {
+        if (Boolean.FALSE.equals(validateUserFeedback(userFeedback))) {
+            logger.warn("Received invalid feedback item: {}", userFeedback);
+            return ResponseEntity.badRequest().build();
+        }
+
+        buffer.addToQueue(userFeedback);
+        logger.info("Added feedback item to queue. Current amount of items in queue: {}", buffer.getSize());
+        return ResponseEntity.accepted().build();
     }
 }
